f97b122e7d5fd3a463542a72ed9ee5e0ef5e1b6e
jaehyun <wogus7an@gmail.com>
2021-10-27 19:33:27 +0900

    reusable composable function

---
+++ vue-composition-api/src/composables/getPosts.js
@@ -0,0 +1,28 @@
+import { ref } from "vue";
+
+const getPosts = () => {
+  // "ref" is for react value inside setup
+  const posts = ref([]);
+  const error = ref(null);
+
+  const load = async () => {
+    try {
+      let data = await fetch("http://localhost:3000/posts");
+      if (!data.ok) {
+        throw Error("no data available");
+      }
+      posts.value = await data.json();
+    } catch (err) {
+      error.value = err.message;
+      console.log(error.value);
+    }
+  };
+
+  return {
+    posts,
+    error,
+    load
+  }
+}
+
+export default getPosts;
--- vue-composition-api/src/views/Home.vue
+++ vue-composition-api/src/views/Home.vue
@@ -3,41 +3,29 @@
     <h1>Home</h1>
     <div v-if="error">{{ error }}</div>
     <div v-if="posts.length">
-      <PostList v-if="showPosts" :posts="posts" />
+      <PostList :posts="posts" />
     </div>
     <div v-else>Loading...</div>
-    <button @click="showPosts = !showPosts">toggle posts</button>
-    <button @click="posts.pop()">delete a post</button>
+    <!-- <button @click="showPosts = !showPosts">toggle posts</button>
+    <button @click="posts.pop()">delete a post</button> -->
   </div>
 </template>
 
 <script>
 import PostList from "../components/PostList.vue";
-import { ref } from "vue";
+import getPosts from "../composables/getPosts";
 
 export default {
   name: "Home",
   components: { PostList },
   setup() {
-    // "ref" is for react value inside setup
-    const posts = ref([]);
-    const error = ref(null);
-    const showPosts = ref(true);
+    const { posts, error, load } = getPosts();
 
-    const load = async () => {
-      try {
-        let data = await fetch("http://localhost:3000/posts");
-        if (!data.ok) {
-          throw Error("no data available");
-        }
-        posts.value = await data.json();
-      } catch (err) {
-        error.value = err.message;
-        console.log(error.value);
-      }
-    };
     load();
-    return { posts, showPosts };
+
+    console.log(posts);
+
+    return { posts, error };
   },
 };
 </script>
